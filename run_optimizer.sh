f5119e533275cac4be6caf5579ce085d182cb858
lukasz-migas <lukas.migas@yahoo.com>
2023-07-17 12:11:32 +0100

    Optimize images

---
+++ run_optimizer.sh
@@ -0,0 +1,8 @@
+# Optimize images in a directory using the given optimizer
+
+optimize-images docs/assets -ca
+
+# iterate through all .md files in the docs directory and replate all .png with .jpg
+# for file in docs/*.md; do
+#     sed 's/\.png/\.jpg/g' $file > $file
+# done
